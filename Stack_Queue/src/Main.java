b7a9090efa1348229f0cb06b27f83287b26dbd64
GezimC <gezim.ciriku@keds-energy.com>
2024-06-03 11:25:12 +0200

    BST

--- Stack_Queue/src/Main.java
+++ Stack_Queue/src/Main.java
@@ -1,30 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        Stack stack = new Stack(8);
-        // Stack: 8
+        Stack stack = new Stack(5);
 
         stack.push(1);
-        // Stack: 1, 8
+        stack.push(2);
+        stack.push(3);
 
-        stack.push(10);
-        // Stack: 10, 1, 8
-
-        stack.push(20);
-        // Stack: 20, 10, 1, 8
-
-        stack.push(30);
-        // Stack: 30, 20, 10, 1,  8
-
-        //show stack elements
-        stack.displayStack();
+//        stack.displayStructure();
 
 
-        //remove top element
-        int top = stack.pop();  //removes top:30
-        System.out.println("\nAfter popping top element: " + top);
-        stack.displayStack(); // 20, 10, 1, 8
+        Queue queue = new Queue(50);
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+
+       // queue.displayStructure();
+        queue.dequeue();
+       // queue.displayStructure();
+
+
+        System.out.println(queue);
+        System.out.println(stack);
 
     }
 }
--- Stack_Queue/src/Stack.java
+++ Stack_Queue/src/Stack.java
@@ -1,17 +1,13 @@
-public class Stack {
+public class Stack extends BaseStructureQS {
 
-    public Element top;
-    public int size;
-    public int max_size = 100;
 
     public Stack(int data) {
-        this.top = new Element(data);
-        this.size = 1;
+        super(data);
     }
 
 
     public void push(int data) {
-        if (size == max_size) {
+        if (this.size == this.max_size) {
             System.out.println("Stack is full");
         }
 
@@ -35,30 +31,8 @@ public class Stack {
         return actualTop;
     }
 
-    public boolean isEmpty() {
-        if (size == 0) return true;
-
-        return false;
+    @Override
+    public String toString() {
+        return "Size of stack is: "+ this.size;
     }
-
-    public boolean isFull() {
-        if (this.size == max_size) return true;
-
-        return false;
-    }
-
-
-    public void displayStack() {
-        int i = 0;
-
-        Element tempElement = this.top;
-
-        while (tempElement != null) {
-            System.out.printf("Elementi %d: %d \n", i, tempElement.data);
-            tempElement = tempElement.next;
-            i++;
-        }
-    }
-
-
 }
